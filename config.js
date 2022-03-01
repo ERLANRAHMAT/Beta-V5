1caf352b5dd6bdde87d6aaeb785a12d73b6ac0dd
ERLANRAHMAT <erlamrahmatdwisaputra@gmail.com>
2022-03-01 07:44:17 +0700

    Beta-V5

---
+++ config.js
@@ -0,0 +1,43 @@
+/**
+   * Base Ori By : Dika Ardnt.
+   * Recode By : RyukaTeam
+   * Source Base : https://github.com/DikaArdnt
+   * Source Recode : https://youtube.com/channel/UCjxavxEQa1Wd9A4J9tOmATA
+   * No Enc? Ga Dijual Om 🤣
+   * Gausah Reupload Kontol!
+   * */
+
+const fs = require('fs')
+const chalk = require('chalk')
+const setting = JSON.parse(fs.readFileSync('./setting.json'))
+    
+    //===> GAUSAH DIUBAH
+    global.owner = [`${setting.nomerowner}`,`${setting.nomerowner2}`,`${setting.nomerowner3}`] //Kalo Mau Lebih Tambahin Aja Dari Setting.json nya
+    global.packname = `${setting.namaowner}`
+    global.author = `© ${setting.namaowner}`
+    global.sessionName = `${setting.sessionname}`
+    global.donasi = `${setting.donasi}`
+    global.prefa = ['','!','/','#','.']
+    global.sp = '⭔'
+    
+    //===> EDIT SESUAI KEMAUANMU
+    global.mess = {
+    admin: '*KAMU BUKAN ADMIN KAK*',
+    botAdmin: '*BOT BUKAN ADMIN 😔*',
+    owner: '*MAAF KAMU BUKAM OWNERKU*',
+    group: '*MAAF KA FITUR INI KHUSUS GROUP*',
+    private: '*KHUSUS PRIVATE CHAT*',
+    bot: '*KHUSUS PENGGUNA NOMER BOT*',
+    wait: '*SABAR SEDANG DI PROSES ...*',
+    done: '*DONE KAKA🤗*',
+    }
+    global.thumb = fs.readFileSync('./media/ryuu.jpg')
+    
+    //===> INI JUGA GAUSAH DIUBAH
+    let file = require.resolve(__filename)
+    fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+    })
